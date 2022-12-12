955727bdcf793ce9abec13d6887824d7295a6227
Teistail <teistail@gmail.com>
2022-12-12 23:08:06 +0300

    setup player and muve

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Runner
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float forwardSpeed = 5f;
+        [SerializeField] private float roadWidth = 5f;
+        [SerializeField] private float turnRotationAngle = 30f;
+        [SerializeField] private float lerpSpeed = 5f;
+        [SerializeField] private Transform model;
+        
+        private Rigidbody _rigidbody;
+        private Animator _animator;
+        private InputHandler _inputHandler;
+
+        private bool _isActive;
+        private static readonly int Run = Animator.StringToHash("Run");
+        private static readonly int Fall = Animator.StringToHash("Fall");
+        private static readonly int Dance = Animator.StringToHash("Dance");
+
+        public bool IsActive
+        {
+            get => _isActive;
+
+            set
+            {
+                _isActive = value;
+                if (_isActive)
+                {
+                    _animator.SetTrigger(Run);
+                }
+            }
+        }
+
+        private void Awake()
+        {
+            _animator = GetComponentInChildren<Animator>();
+            _inputHandler = GetComponentInChildren<InputHandler>();
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Start()
+        {
+            IsActive = true;
+        }
+        
+        private void FixedUpdate()
+        {
+            if(!IsActive)
+                return;
+            Move();
+        }
+
+        private void Move()
+        {
+            var xOffseet = -_inputHandler.HorizontalAxis * roadWidth;
+            var position = _rigidbody.position;
+            position.x += xOffseet;
+
+            var rotation = model.localRotation.eulerAngles;
+            rotation.y = Mathf.LerpAngle(rotation.y, xOffseet == 0 ? 0 : Mathf.Sign(xOffseet) * turnRotationAngle,
+                lerpSpeed * Time.deltaTime);
+            model.localRotation = Quaternion.Euler(rotation);
+            
+            _rigidbody.MovePosition(position + transform.forward * (forwardSpeed * Time.deltaTime));
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.GetComponent<WallComponent>())
+            {
+                Died();
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.GetComponent<FinishComponent>())
+            {
+                Finish();
+            }
+        }
+
+        [ContextMenu("Died")]
+        private void Died()
+        {
+            IsActive = false;
+            _animator.SetTrigger(Fall);
+        }
+
+        [ContextMenu("Finish")]
+        private void Finish()
+        {
+            _isActive = false;
+            _animator.SetTrigger(Dance);
+        }
+    }
+}
