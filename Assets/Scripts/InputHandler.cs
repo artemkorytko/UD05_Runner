5346717e6a363765ebb72efbf49a49633ddfdb53
HOME-PC\JUST <roman.raven96@mail.ru>
2022-12-14 19:24:31 +0300

    Inprogress backup

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace Runner
+{
+    public class InputHandler : MonoBehaviour
+    {
+        private bool _isHold;
+        private float _prevPosX;
+        private float _relativeOffset;
+        private int _screenWidth;
+
+        public float HorizontalAxis => _relativeOffset;
+
+        private void Awake()
+        {
+            _screenWidth = Screen.width;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _isHold = true;
+                _prevPosX = Input.mousePosition.x;
+            }
+            
+            if (Input.GetMouseButtonUp(0))
+            {
+                _isHold = false;
+                _prevPosX = 0;
+            }
+            
+            if (_isHold)
+            {
+                var mousePos = Input.mousePosition.x;
+                var offset = _prevPosX - mousePos;
+                _relativeOffset = offset / _screenWidth;
+                _prevPosX = mousePos;
+            }
+        }
+    }
+}
