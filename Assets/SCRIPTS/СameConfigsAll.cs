63091fefcadb9eaecf0e54b207d399dd2e537e97
Pinksharkapps <pinksharkapps@gmail.com>
2023-01-19 10:41:14 +0300

    Number on Coins shown (from class field). Before configs adding!

---
+++ Assets/SCRIPTS/СameConfigsAll.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Runner
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class СameConfigsAll : ScriptableObject
+    {
+        
+    }
+}
